e9b096800b60ef26b6a2d9f4b47273272acb6ba1
QuimMarset <quim1996@hotmail.es>
2017-12-17 23:37:20 +0100

    Improving predictor

--- src/prediction/getEyesFromScores.m
+++ src/prediction/getEyesFromScores.m
@@ -1,18 +1,18 @@
 function [pos1, pos2] = getEyesFromScores(scores, imageSize, rectSize)
     scores = horzcat(scores, (1:length(scores))');
-    scores = sortrows(scores, 'descend');
+    scores = sortrows(scores, -1);
     positions = scores(:, 2);
     % First eye
     p1 = positions(1);
-    i1 = floor(p1 / ((imageSize - rectSize) / 5));
-    j1 = mod(p1, (imageSize - rectSize) / 5);
+    i1 = floor(p1 / ((imageSize - rectSize) / 2));
+    j1 = mod(p1, (imageSize - rectSize));
     pos1 = [i1, j1];
     % Second eye
     pos2 = [1, 1];
     for p = 2 : length(positions)
         p2 = positions(p);
-        i2 = floor(p2 / (imageSize - rectSize)) * 5;
-        j2 = mod(p2, (imageSize - rectSize) / 5);
+        i2 = floor(p2 / (imageSize - rectSize)) * 2;
+        j2 = mod(p2, (imageSize - rectSize));
         if (i2 < i1 - rectSize/3 || i2 > i1 + rectSize/3) || ...
            (j2 < j1 - rectSize/3 || j2 > j1 + rectSize/3)
             pos2 = [i2, j2];
