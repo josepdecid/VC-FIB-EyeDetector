d2544532e3ba63a4bc51a1a328a83e8b51e08cb0
JosepRIvaille <josep.de.cid@gmail.com>
2017-12-18 00:11:07 +0100

    Add steps on rows and columns

--- src/prediction.m
+++ src/prediction.m
@@ -1,5 +1,6 @@
-%pred = trainPredictor(eyes, noEyes);
+% pred = trainPredictor(eyes, noEyes);
 
+steps = 2;
 rectSize = 30;
 rectSizeVec = [rectSize, rectSize];
 
@@ -12,9 +13,9 @@ predictedPositions = zeros(length(testImages), 4);
 %    predictedPositions(i, :) = [pos1, pos2];
 %end
 
-[~, scores] = predictImages(pred, testImages(:, :, 99), imageSize, rectSize);
-[pos1, pos2] = getEyesFromScores(scores(:, 1), imageSize, rectSize);
-I = uint8(squeeze(testImages(:, :, 99)));
+[~, scores] = predictImages(pred, testImages(:, :, 1), imageSize, rectSize, steps);
+[pos1, pos2] = getEyesFromScores(scores(:, 1), imageSize, rectSize, steps);
+I = uint8(squeeze(testImages(:, :, 1)));
 I = insertShape(I, 'rectangle', [pos1, rectSize, rectSize], 'LineWidth', 1);
 I = insertShape(I, 'rectangle', [pos2, rectSize, rectSize], 'LineWidth', 1);
 imshow(I);
--- src/prediction/predictImages.m
+++ src/prediction/predictImages.m
@@ -1,11 +1,11 @@
-function [label, scores] = predictImages(pred, image, imageSize, rectSize)
+function [label, scores] = predictImages(pred, image, imageSize, rectSize, steps)
     % TODO: 10 subdivisions (HELL 2 U ARBITRARI) Es pot fer amb un metode?
     resize = 64;
-    subImages = zeros([resize, resize, (imageSize - rectSize)*(imageSize - rectSize)/2]);
+    subImages = zeros([resize, resize, ((imageSize - rectSize)/steps)^2]);
     k = 1;
-    for i = 1:(imageSize - rectSize)/2
-        for j = 1:(imageSize - rectSize)
-            subImage = imcrop(image, [i*2, j, rectSize, rectSize]);
+    for i = 1:steps:(imageSize - rectSize)
+        for j = 1:steps:(imageSize - rectSize)
+            subImage = imcrop(image, [i, j, rectSize, rectSize]);
             subImages(:, :, k) = imresize(subImage, [resize, resize]);
             k = k + 1;
         end
