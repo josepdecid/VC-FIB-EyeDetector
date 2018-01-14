8441d492cbdc85129052c78310815f0409f7df8e
JosepRIvaille <josep.de.cid@gmail.com>
2018-01-14 12:16:21 +0100

    Optimize KNN model

--- src/prediction/predictors/knn.m
+++ src/prediction/predictors/knn.m
@@ -1,19 +1,31 @@
-function [classifier, error] = knn(Tbl, Labels)
-    neighbours = [5, 9, 11, 13, 15];
-    classifiers = cell(length(neighbours), 1);
-    errors = zeros(length(neighbours), 1);
-    
-    for i = 1:length(neighbours)
-        classifiers{i} = fitcknn(          ...
-            Tbl, Labels,                   ...
-            'NumNeighbors', neighbours(i), ...
-            'Distance', 'seuclidean');
-        
-        CVal = crossval(classifiers{i});
-        errors(i) = kfoldLoss(CVal);
+function [classifier] = knn(Tbl, Labels, K, Dist)
+    if K == -1 || Dist == ''
+        neighbours = [5, 9, 11, 13, 15];
+        distances = ["euclidean", "seuclidean", "mahalanobis"];
+
+        classifiers = cell(length(neighbours), length(distances));
+        errors = zeros(length(neighbours), length(distances));
+
+        for i = 1:length(neighbours)
+            for j = 1:length(distances)
+                classifiers{i, j} = fitcknn(       ...
+                    Tbl, Labels,                   ...
+                    'NumNeighbors', neighbours(i), ...
+                    'Distance', char(distances(j)));
+
+                CVal = crossval(classifiers{i, j});
+                errors(i, j) = kfoldLoss(CVal);
+            end
+        end
+
+        [~, idx] = min(errors(:));
+        [minR, minC] = ind2sub(size(errors), idx);
+        classifier = classifiers{minR, minC};
+    else
+        classifier = fitcknn(  ...
+            Tbl, Labels,       ...
+            'NumNeighbors', K, ...
+            'Distance', Dist);
     end
-    
-    [error, i] = min(errors);
-    classifier = classifiers{i};
 end
 
