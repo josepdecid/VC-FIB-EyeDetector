8ea405c60c4b03a213251c7c01bdb2f1f2e65228
JosepRIvaille <josep.de.cid@gmail.com>
2018-01-16 00:54:59 +0100

    Full code? (maybe)

--- src/prediction.m
+++ src/prediction.m
@@ -8,6 +8,7 @@ testImages = testset{1};
 testEyesPos = testset{2};
 testEyesLook = testset{3};
 looksPrediction = zeros(100, 1);
+predictedPositions = zeros(length(testImages), 4);
 
 % Get test set eyes and noEyes and test
 testEyes = zeros(dimensions.subImgSize, dimensions.subImgSize, 2 * length(testImages));
@@ -65,29 +66,15 @@ end
 accuracyLook = sum(diag(confLook)) / sum(sum(confLook));
 accuracyLookF1 = 2 / sum(1 ./ (diag(confLook) ./ sum(confLook, 2)));
 
-%{
-for i = 1:100
+% Display predicted images
+for i = 1:length(testImages)
     disp(i);
     
-    [~, scores] = predictEyes(predEyes, testImages(:, :, i), dimensions, rectSize);
-    confPos = getMatrixFromScores(confPos, scores, testEyesPos, dimensions, rectSize);
+    [~, scores] = predictEyes(predEyes, testImages(:, :, i), dimensions, rectSize); 
     [pos1, pos2] = getEyesFromScores(scores(:, 1), dimensions, rectSize);
     predictedPositions(i, :) = [pos1, pos2];
-end
-
-accuracyPos = sum(diag(confPos)) / sum(sum(confPos));
-accuracyPosF1 = 2 / sum(1 ./ (diag(confPos) ./ sum(confPos, 2)));
-
-%predictedPositions = testEyesPos;
-%confLook = zeros(2, 2);
-%for i = 1:length(testImages)
-%    disp(i);
     
-%    looksPrediction(i, :) = predictLooks(predLooks, testImages(:, :, i), ...
-%        testEyesPos(i, :), dimensions);
-%    confLook = getMatrixFromLooks(confLook, looksPrediction(i, :), testEyesLook(i));
-%end
-
-%accuracyLook = sum(diag(confLook)) / sum(sum(confLook));
-%accuracyLookF1 = 2 / sum(1 ./ (diag(confLook) ./ sum(confLook, 2)));
-%}
+    looksPrediction(i, :) = predictLooks(predLooks, testImages(:, :, i), ...
+        predictedPositions(i, :), dimensions);
+    confLook = getMatrixFromLooks(confLook, looksPrediction(i, :), testEyesLook(i));
+end
--- src/prediction/predictEyes/trainPredictorEyes.m
+++ src/prediction/predictEyes/trainPredictorEyes.m
@@ -13,6 +13,6 @@ function [classifier] = trainPredictorEyes(eyes, noEyes)
     % end
     % Tbl = scores(:, 1:i);
     
-    classifier = randomForest(Tbl, Labels', 63);
+    classifier = svm(Tbl, Labels', 4.5384, 1.6061);
 end
 
